4a28dd8d9c2464e10a6d9f6d61a4fa623409e410
JayJ <jorgenstenshaugen@gmail.com>
2019-08-25 21:11:38 +0200

    Fixed a small int bug

--- Oblig 1/dato2.py
+++ Oblig 1/dato2.py
@@ -1,13 +1,22 @@
 # Simplifisert fra "dato.py"
 # Her henter vi datoen i en dag.mnd format og splitter verdien vekk fra punktummet.
-dato1 = int(input("Skriv inn første dato (eks. 7.10): ").split("."))
-dato2 = int(input("Skriv inn ny dato (eks. 24.12): ").split("."))
+dato1 = input("Skriv inn første dato (eks. 7.10): ").split(".")
+dato2 = input("Skriv inn ny dato (eks. 24.12): ").split(".")
 
 # Vi henter verdiene gjennom et array og utfør samme funksjon som i "dato.py"
-# dag1 = dato1[0], dag2 = dato2[0]. maaned1 = dato1[1], maaned2 = dato2[1]
-if dato1[0] < dato2[0] and dato1[1] <= dato2[1] :
+# Bruker int() for å sikkre at verdiene blir tatt inn som heltall.
+dag1 = int(dato1[0])
+dag2 = int(dato2[0])
+maaned1 = int(dato1[1])
+maaned2 = int(dato2[1])
+
+if dag1 < dag2 and maaned1 <= maaned2 :
+    # Sjekker om dag1 er før dag2 og om maaned1 er før eller den samme.
+    # Deretter skriver vi ut at datoen er i riktig rekkefølge
     print("Riktig rekkefølge")
-elif dato1[0] == dato2[0] and dato1[1] == dato2[1] :
+elif dag1 == dag2 and maaned1 == maaned2 :
+    # Her sjekker vi om datoene er helt like og skriver ut en melding.
     print("Samme dato!")
 else :
+    # Dersom dag1 og maaned1 er etter dag2 og maaned2 skriver vi ut en feilmelding
     print("feil rekkefølge")
