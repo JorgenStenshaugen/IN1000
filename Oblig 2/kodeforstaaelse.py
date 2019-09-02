68b0c14165d76a557eb78719d4afb1e1529fa8c8
leJayJ <jorgenstenshaugen@gmail.com>
2019-09-02 11:41:52 +0200

    Added rest of oblig 2

---
+++ Oblig 2/kodeforstaaelse.py
@@ -0,0 +1,7 @@
+a = input("Tast inn et heltall! ")
+b = int(a)
+if b < 10:
+    print (b + "Hei!")
+
+# 1. Dette er en lovlig kode siden den vil kjøre uten problemer dersom brukeren skriver inn et tall over 10
+# 2. Dersom brukeren skriver inn et tall under 10 vil det komme en feil siden b ikke lenger er en string.
