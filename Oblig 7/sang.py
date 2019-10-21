571c00fa3d02d77183cb40d146bfc1b8551bbf59
leJayJ <jorgenstenshaugen@gmail.com>
2019-10-21 21:56:15 +0200

    Updated oblig7 with comments

--- Oblig 7/sang.py
+++ Oblig 7/sang.py
@@ -1,27 +1,45 @@
 class Sang:
-
+    # Instansmetode med 2 parametere, "tittel" og "artist".
     def __init__(self, tittel, artist):
+        # Instansvariabelene
         self._tittel = tittel
         self._artist = artist
 
+    # Metode som printer ut Instansvariabelene
     def spill(self):
         print("Spiller av", self._tittel, "-", self._artist)
 
+    # Funksjonsmetode som tar inn parameteren "navn"
     def sjekkArtist(self, navn):
-        for ord in navn.split():
-            for artistOrd in self._artist.split():
-                if ord == artistOrd:
+        # For loop som går gjennom "navn" splittet opp
+        # Slik at den sjekker alle mellomnavn osv.
+        for navnSplittet in navn.split():
+            # For loop som går gjennom navnene på selve artisten splittet opp.
+            for artistNavn in self._artist.split():
+                # Dersom navnSplittet matcher en av en av "artistNavn"ene.
+                # Så returnerer den True.
+                if navnSplittet == artistNavn:
                     return True
+        # Dersom for løkken ovenfor ikke returner True, så returnerer funksjonen False
         return False
 
-
+    # Funksjonsmetode som tar in parameteren "tittel"
     def sjekkTittel(self, tittel):
+        # For løkke som går gjennom ordene i "tittel".
         for ord in tittel.split():
+            # For løkke som går gjennom alle ordene i tittelen til dette objektet.
             for tittelOrd in self._tittel.split():
+                # Sjekker om ordet matcher objektets ord i tittelen.
+                # Gjør begge til småbokstaver slik at stor og små bokstav ikke
+                # påvirker sjekken. Dersom det matcher så returnerer vi True.
                 if ord.lower() == tittelOrd.lower():
                     return True
+        # Om ikke for løkken returnerer noe så returnerer vi False.
         return False
 
+    # Funksjonsmetode som tar inn 2 parametere, "artist" og "tittel".
     def sjekkArtistogTittel(self, artist, tittel):
-        if self.sjekkArtist(artist) and self.sjekkTittel(tittel):
-            return True
+        # Bruker funksjonsmetodene sjekkArtist og "sjekkTittel"
+        # Dersom begge metodene returnerer True, så returnerer denne metoden True.
+        # Ellers så returnerer den False. (True and True = True, False and True = False)
+        return self.sjekkArtist(artist) and self.sjekkTittel(tittel)
--- Oblig 7/spilleliste.py
+++ Oblig 7/spilleliste.py
@@ -1,44 +1,66 @@
 from sang import Sang
 
 class Spilleliste:
+    # Instansmetode som tar in parameteren "listenavn"
     def __init__(self, listenavn):
+        # Setter instansvariabelen _sanger til en liste
+        # og setter _navn til listenavn argumentet.
         self._sanger = []
         self._navn = listenavn
 
+    # Metode som tar inn "filnavn" som parameter
     def lesFraFil(self, filnavn):
-        data = open(filnavn)
+        # Åpner filen
+        sangFil = open(filnavn)
 
-        for linje in data.read().split("\n"):
+        # For løkke som går gjennom hver linje og legger
+        # sang objektet med tittel og artist i "_sanger" listen.
+        for linje in sangFil.read().split("\n"):
             tittel = linje.split(";")[0]
             artist = linje.split(";")[1]
             self._sanger.append(Sang(tittel, artist))
+        # Lukker filen.
+        sangFil.close()
 
-        data.close()
 
-
+    # Metode som tar inn parameteren "nySang" og legger den i "_sanger" listen.
     def leggTilSang(self, nySang):
         self._sanger.append(nySang)
 
+    # Metode som tar inn parameteren "sang" og fjerner den fra listen.
     def fjernSang(self, sang):
         self._sanger.remove(sang)
 
+    # Metode som tar inn en parameter sang, som skal være et objekt og
+    # kaller det objektets "spill()" metode.
     def spillSang(self, sang):
         sang.spill()
 
+    # Metode som går gjennom alle sang objektene i "_sanger" listen og
+    # kaller på objektets "spill()" metode.
     def spillAlle(self):
         for sang in self._sanger:
             sang.spill()
 
+    # Funksjonsmetode som tar inn parameteret "tittel"
     def finnSang(self, tittel):
+        # Går gjennom for løkke som går gjennom alle objektene i "_sanger" lista.
         for sang in self._sanger:
+            # Dersom objektets "sjekkTittel" metode returnerer True, så returnerer vi sang objektet.
             if sang.sjekkTittel(tittel):
                 return sang
+        # Dersom for løkken ikke returnerer noe så returnerer vi None.
         return None
 
+    # Funksjonsmetode som tar inn parameteren "artistnavn"
     def hentArtistUtvalg(self, artistnavn):
+        # Oppretter enn ny liste som skal ta inn nye Sang objekter.
         nyListe = []
+        # For løkke som går gjennom "_sanger" listen.
         for sang in self._sanger:
+            # Dersom objektets metode "SjekkArtist" returnerer True så legger vi
+            # objektet i den nye listen.
             if sang.sjekkArtist(artistnavn):
                 nyListe.append(sang)
-
+        # Når løkken er ferdig så returnerer vi den nye listen.
         return nyListe
